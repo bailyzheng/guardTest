f898d694bf34752914d24fce34a4e9b9f4e9a6ca
zhenglei <1394694290@qq.com>
2020-07-03 17:36:22 +0800

    doing

--- app/src/main/java/com/honghe/guardtest/LocalService.kt
+++ app/src/main/java/com/honghe/guardtest/LocalService.kt
@@ -1,15 +1,22 @@
 package com.honghe.guardtest
 
+import android.app.ActivityManager
+import android.app.AlarmManager
+import android.app.PendingIntent
 import android.app.Service
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
+import android.os.Build
 import android.os.IBinder
+import android.os.Process
 import android.os.RemoteException
 import android.util.Log
 import android.widget.Toast
 import com.honghe.guardtest.LocalService
+import java.util.*
+import kotlin.system.exitProcess
 
 class LocalService : Service() {
     private var mBinder: MyBinder? = null
@@ -19,9 +26,13 @@ class LocalService : Service() {
             try {
                 Log.e("LocalService", "connected with " + iMyAidlInterface.serviceName)
                 if (MyApplication.mainActivity == null) {
-                    val intent = Intent(this@LocalService.baseContext, MainActivity::class.java)
-                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-                    application.startActivity(intent)
+                    Log.e("LocalService", "try to start MainActivity")
+                    restartApp(this@LocalService)
+//                    val intent = Intent(this@LocalService.baseContext, MainActivity::class.java)
+//                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+//                    application.startActivity(intent)
+                } else {
+                    Log.e("LocalService", "MainActivity started")
                 }
             } catch (e: RemoteException) {
                 e.printStackTrace()
@@ -34,6 +45,34 @@ class LocalService : Service() {
             startService(Intent(this@LocalService, RemoteService::class.java))
             bindService(Intent(this@LocalService, RemoteService::class.java), this, Context.BIND_IMPORTANT)
         }
+    }
+
+    fun restartApp(context: Context) {
+        val outIntent = context.packageManager.getLaunchIntentForPackage(context.packageName)
+        outIntent?.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
+        val mPendingIntentId = Random().nextInt()
+        val mPendingIntent = PendingIntent.getActivity(context, mPendingIntentId, outIntent, PendingIntent.FLAG_CANCEL_CURRENT)
+        val mgr = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        mgr.set(AlarmManager.RTC, System.currentTimeMillis() + 500, mPendingIntent)
+        exitApp(context)
+
+//            val outIntent = context.packageManager.getLaunchIntentForPackage(context.packageName)
+//            outIntent?.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
+//            context.startActivity(outIntent)
+
+//            System.exit(-1)
+    }
+
+    fun exitApp(context: Context) {
+        val activityManager = context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val appTaskList = activityManager.appTasks
+            for (appTask in appTaskList) {
+                appTask.finishAndRemoveTask()
+            }
+        }
+        Process.killProcess(Process.myPid())
+        exitProcess(0)
     }
 
     override fun onCreate() {
